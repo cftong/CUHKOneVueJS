6cb6297ad04c82aff6576820c7897b34b920aa4f
TONG Cham Fei <cftong@outlook.com>
2017-01-05 18:31:17 +0800

    About.vue: Content Update

--- src/template/About.vue
+++ src/template/About.vue
@@ -3,7 +3,7 @@
   <content>
     <div class="content-padded">
       <p>免責聲明:本網站內容由相關網頁即時擷取。開發人員無須事先通知而可隨時修改本網頁的任何部分。因使用、誤用或依據此網頁的資訊而導致的損失或破壞，開發人員概不負責。</p>
-      <p>Disclaimer:The contents of the website of CUOne is captured from the related websites. Developer is subject to change without notice. The developer accepts no liability for any loss or damage howsoever arising from any use or misuse of or reliance on any information in this website.</p>
+      <p>Disclaimer:The contents of the website of CUOne is captured from the related websites in real time. Developer is subject to change without notice. The developer accepts no liability for any loss or damage howsoever arising from any use or misuse of or reliance on any information in this website.</p>
       <p>Developed by Chris Tong</p>
       <p><a href="https://blogcftong.azurewebsites.net">My Blog</a></p>
       <p>The UI is based on VUM which is an UI Framework for build mobile web app with vue.js by 言川</p>
